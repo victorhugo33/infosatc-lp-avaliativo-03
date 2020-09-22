ec878447670a7ec539a5e821970bc2198a11dffb
Victor Hugo <69268538+victorhugo33@users.noreply.github.com>
2020-09-22 16:55:37 -0300

    Create exercicio04.py

---
+++ https:/github.com/exercicio04.py
@@ -0,0 +1,6 @@
+sum_of_numbers = 0
+for x in range(1,101): sum_of_numbers+=x
+print("\nA soma dos números entre 1 a 100 é:", sum_of_numbers)
+print("\nTente : sn = n(a1 + a2)            Com sn sendo a soma dos termos       ")
+print("                         ----------            a1 o primeiro e an o último    ")
+print("                             2                 por fim, n é o número total de:")
